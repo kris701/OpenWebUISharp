aca02d8d58308b41875f42428c7e598d9c4748a2
Kristian Skov <kris701kj@gmail.com>
2026-07-28 10:11:05 +0200

    Updated workflows and bumped openwebui supported version

--- OpenWebUISharp.Tests/APIConfiguration.cs
+++ OpenWebUISharp.Tests/APIConfiguration.cs
@@ -3,6 +3,6 @@
 	public static class APIConfiguration
 	{
 		public static string APIURL = "http://localhost:3030";
-		public static string APIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjlhOTlmMjNkLTVlYmMtNDMzMy05NDk0LTMyZjNiOWQ2YzRkMiIsImp0aSI6ImU3OWZlY2RhLTE3YjgtNDAxZC1hMWMxLTAwYWYwNzQyNGFjMSIsImlhdCI6MTc3ODE0MjA5MX0.JVOHbSYzJVgfljnd2VCn3_6J8yLXFmjFLnvcYq3yLcs";
+		public static string APIKey = "sk-3b68286432464dc2800d7b7cae3d47d6";
 	}
 }
